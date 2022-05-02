bb7a562af77189a202574d14a68b968c02232520
LKM <kangmin7434@naver.com>
2022-05-02 01:21:05 +0000

    FIX: temp code

--- inference.sh
+++ inference.sh
@@ -1,19 +1,19 @@
-python inference.py \
---output_dir ./outputs/test_dataset/ \
---model_name_or_path ./models/train_dataset/ \
---dataset_name ../data/test_dataset/ \
---do_predict \
---overwrite_output_dir True \
---predict_file_name 4990 \
+# python inference.py \
+# --output_dir ./outputs/test_dataset/ \
+# --model_name_or_path ./models/train_dataset/ \
+# --dataset_name ../data/test_dataset/ \
+# --do_predict \
+# --overwrite_output_dir True \
+# --predict_file_name 1996 \
 
 
 python inference.py \
 --output_dir ./outputs/test_dataset/ \
---model_name_or_path ./models/train_dataset/checkpoint-3500/ \
+--model_name_or_path ./models/train_dataset/checkpoint-1500/ \
 --dataset_name ../data/test_dataset/ \
 --do_predict \
 --overwrite_output_dir True \
---predict_file_name 3500 \
+--predict_file_name 1500 \
 
 
 # --model_name_or_path ./models/train_dataset/checkpoint-1000/ \
--- train.sh
+++ train.sh
@@ -4,15 +4,16 @@ python train.py \
 --do_eval \
 --overwrite_output_dir True \
 --fp16 True \
---per_device_train_batch_size 16 \
---per_device_eval_batch_size 16 \
---num_train_epochs 10 \
+--per_device_train_batch_size 8 \
+--per_device_eval_batch_size 8 \
+--num_train_epochs 2 \
 --weight_decay 0.01 \
 --warmup_ratio 0.1 \
---learning_rate 6e-6 \
---project_name "[MRC] baseline" \
+--learning_rate 3e-5 \
+--project_name "[MRC] hp-tuning" \
 --entity_name growing_sesame \
---wandb_run_name "robert-large batch 16 epoch 10" \
+--wandb_run_name "[lkm] robert-large 2 epoch 3e5" \
 --evaluation_strategy steps \
 --eval_steps 500 \
 --save_total_limit 5 \
+
