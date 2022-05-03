7a7ca8c2ba3085e898cf19c3e35436368b2e7e26
LKM <kangmin7434@naver.com>
2022-05-03 12:17:05 +0000

    FIX:pull master

--- inference.sh
+++ inference.sh
@@ -1,4 +1,14 @@
-python inference.py \
+# python inference.py \
+# --output_dir ./outputs/test_dataset/ \
+# --model_name_or_path ./models/train_dataset/ \
+# --overwrite_output_dir True \
+# --predict_file_name 9980 \
+# --top_k_retrieval 20 \
+# --dataset_name ../data/test_dataset/ \
+# --do_predict \
+
+
+python retrieval_test.py \
 --output_dir ./outputs/test_dataset/ \
 --model_name_or_path ./models/train_dataset/ \
 --overwrite_output_dir True \
