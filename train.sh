6c1a36e43d8b473e3b6bae99688ad1c67d778ef3
LKM <kangmin7434@naver.com>
2022-05-08 09:33:27 +0000

    FIX: save problem

--- train.sh
+++ train.sh
@@ -11,7 +11,6 @@ python train.py \
 --warmup_ratio 0.1 \
 --learning_rate 3e-5 \
 --report_to wandb \
-<<<<<<< HEAD
 --project_name "[MRC] hp-tuning" \
 --entity_name growing_sesame \
 --wandb_run_name "[lkm] roberta-large max512 doc32" \
@@ -22,17 +21,7 @@ python train.py \
 --overwrite_cache \
 --max_seq_length 512 \
 --doc_stride 32 
-=======
---project_name "[MRC] Dense" \
---entity_name hyunah \
---wandb_run_name "[hyunah] bert-base" \
---evaluation_strategy steps \
---eval_steps 500 \
---save_total_limit 5 \
---model_name_or_path "klue/bert-base" \
---overwrite_cache \
 
 # --embedding_type "Dense" 
 # --train_dense_retrieval \
 # --train_retrieval \
->>>>>>> 936629271946de35b483fe868b0a4ea5fdd78adf
